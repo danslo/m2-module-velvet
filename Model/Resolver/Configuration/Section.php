0117a44d1e325fbaaa96462d2902e1925541f325
Daniel Sloof <daniel@wearejh.com>
2021-11-15 16:41:36 +0100

    return config sections

---
+++ Model/Resolver/Configuration/Section.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Danslo\Velvet\Model\Resolver\Configuration;
+
+use Danslo\Velvet\Model\Authorization;
+use Danslo\Velvet\Model\Configuration;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class Section implements ResolverInterface
+{
+    private Configuration $configuration;
+    private Authorization $authorization;
+
+    public function __construct(Configuration $configuration, Authorization $authorization)
+    {
+        $this->configuration = $configuration;
+        $this->authorization = $authorization;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $this->authorization->validate($context);
+
+        $section = $this->configuration->getAdminhtmlConfigStructure()->getElement($args['section']);
+
+        if ($section->hasChildren() === false) {
+            return [];
+        }
+
+        $groups = [];
+        foreach ($section->getChildren() as $group) {
+            $fields = [];
+            foreach ($group->getChildren() as $field) {
+                $fields[] = [
+                    'label' => (string) $field->getLabel(),
+                    'type' => $field->getType(),
+                    'comment' => ((string) $field->getComment()) ?: null
+                ];
+            }
+            $groups[] = [
+                'label' => (string) $group->getLabel(),
+                'fields' => $fields
+            ];
+        }
+
+        return $groups;
+    }
+}
