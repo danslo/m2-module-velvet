fbdec77ced0ebdf2cee14fe7e3858b50c2a24568
Daniel Sloof <daniel@wearejh.com>
2021-11-25 02:02:54 +0100

    do the actual stitching

--- Model/Config/OrderGridReader.php
+++ Model/Config/OrderGridReader.php
@@ -4,13 +4,33 @@ declare(strict_types=1);
 
 namespace Danslo\Velvet\Model\Config;
 
+use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\Config\ReaderInterface;
+use Magento\Sales\Model\ResourceModel\Order\Grid\Collection as OrderGridCollection;
 
 class OrderGridReader implements ReaderInterface
 {
+    private ResourceConnection $resourceConnection;
+    private OrderGridCollection $orderGridCollection;
+
+    public function __construct(ResourceConnection $resourceConnection, OrderGridCollection $orderGridCollection)
+    {
+        $this->resourceConnection = $resourceConnection;
+        $this->orderGridCollection = $orderGridCollection;
+    }
+
     public function read($scope = null)
     {
-        /** @see \Magento\CatalogGraphQl\Model\Config\AttributeReader */
-        return [];
+        $connection = $this->resourceConnection->getConnection();
+        if (!$connection->isTableExists($this->orderGridCollection->getMainTable())) {
+            return [];
+        }
+
+        $config = [];
+        foreach ($connection->describeTable($this->orderGridCollection->getMainTable()) as $column => $description) {
+            $config['GridOrder']['fields'][$column] = ['name' => $column, 'type' => 'String', 'arguments' => []];
+        }
+
+        return $config;
     }
 }
