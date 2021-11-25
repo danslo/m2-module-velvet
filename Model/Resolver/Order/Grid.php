ea9ef95c0574a6f6520294232e01ffbc0315165a
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-25 13:47:43 +0100

    add order grid resolver

--- Model/Resolver/Order/Grid.php
+++ Model/Resolver/Order/Grid.php
@@ -5,36 +5,81 @@ declare(strict_types=1);
 namespace Danslo\Velvet\Model\Resolver\Order;
 
 use Danslo\Velvet\Model\Authorization;
+use Magento\Directory\Model\Currency;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Document;
+use Magento\Sales\Model\ResourceModel\Order\Grid\Collection;
 use Magento\Sales\Model\ResourceModel\Order\Grid\CollectionFactory as OrderGridCollectionFactory;
+use Magento\Store\Model\System\Store as SystemStore;
 
 class Grid implements ResolverInterface
 {
     private Authorization $authorization;
     private OrderGridCollectionFactory $orderGridCollectionFactory;
+    private Currency $currency;
+    private array $currencyFormatFields;
+    private int $defaultPageSize;
+    private SystemStore $store;
 
-    public function __construct(Authorization $authorization, OrderGridCollectionFactory $orderGridCollectionFactory)
-    {
+    public function __construct(
+        Authorization $authorization,
+        OrderGridCollectionFactory $orderGridCollectionFactory,
+        Currency $currency,
+        SystemStore $store,
+        array $currencyFormatFields = [],
+        int $defaultPageSize = 20
+    ) {
         $this->authorization = $authorization;
         $this->orderGridCollectionFactory = $orderGridCollectionFactory;
+        $this->currency = $currency;
+        $this->currencyFormatFields = $currencyFormatFields;
+        $this->defaultPageSize = $defaultPageSize;
+        $this->store = $store;
+    }
+
+    private function getStorenameFromStoreId(int $storeId): string
+    {
+        return $this->store->getStoreName($storeId);
+    }
+
+    private function getOrderData(Document $order): array
+    {
+        $data = $order->getData();
+
+        foreach ($this->currencyFormatFields as $field) {
+            if (!isset($data[$field])) {
+                continue;
+            }
+            $data[$field] = $this->currency->format($data[$field], [], false);
+        }
+
+        $data['store_name'] = $this->getStorenameFromStoreId((int) $data['store_id']);
+
+        return $data;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
         $this->authorization->validate($context);
 
-        /**
-         * TODO:
-         * - pagination
-         * - limit
-         * - sorting
-         */
+        /** @var Collection $collection */
+        $collection = $this->orderGridCollectionFactory->create()
+            ->setCurPage($args['input']['page_number'] ?? 0)
+            ->setPageSize($args['input']['page_size'] ?? $this->defaultPageSize)
+            ->addOrder('created_at'); /** todo: add user-defined sorting */
+
+        // todo: add filters
+
         $orders = [];
-        foreach ($this->orderGridCollectionFactory->create() as $order) {
-            $orders[] = $order->getData();
+        foreach ($collection as $order) {
+            $orders[] = $this->getOrderData($order);
         }
-        return $orders;
+        return [
+            'orders' => $orders,
+            'last_page_number' => $collection->getLastPageNumber(),
+            'total_orders' => $collection->getTotalCount()
+        ];
     }
 }
