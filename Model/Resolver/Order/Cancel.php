d52655de99c279771bb049a33e1591e36b55c208
Daniel Sloof <goapsychadelic@gmail.com>
2021-11-29 13:28:36 +0100

    stub order actions

---
+++ Model/Resolver/Order/Cancel.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Danslo\Velvet\Model\Resolver\Order;
+
+use Danslo\Velvet\Api\AdminAuthorizationInterface;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Sales\Model\OrderRepository;
+
+class Cancel implements ResolverInterface, AdminAuthorizationInterface
+{
+    private OrderRepository $orderRepository;
+
+    public function __construct(OrderRepository $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $orderId = $args['order_id'] ?? null;
+        if ($orderId === null) {
+            throw new GraphQlInputException(__('Required parameter "order_id" is missing'));
+        }
+
+        $order = $this->orderRepository->get($orderId);
+        // TODO: implement
+    }
+}
