bc89aac141c1537e317e80957db9550f7b216745
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-05 14:46:18 +0100

    use the grid generator for everything

--- Model/Config/GridReader.php
+++ Model/Config/GridReader.php
@@ -23,6 +23,23 @@ class GridReader implements ReaderInterface
         $this->schemaType = $schemaType;
     }
 
+    private function getTypeFromColumnDescription(array $description): String
+    {
+        switch ($description['DATA_TYPE']) {
+            case 'tinyint':
+            case 'smallint':
+            case 'int':
+            case 'mediumint':
+            case 'bigint':
+                return 'Int';
+            case 'decimal':
+            case 'numeric':
+                return 'Float';
+            default:
+                return 'String';
+        }
+    }
+
     public function read($scope = null)
     {
         $connection = $this->resourceConnection->getConnection();
@@ -32,7 +49,12 @@ class GridReader implements ReaderInterface
 
         $config = [];
         foreach ($connection->describeTable($this->tableName) as $column => $description) {
-            $config[$this->schemaType]['fields'][$column] = ['name' => $column, 'type' => 'String', 'arguments' => []];
+            $config[$this->schemaType]['fields'][$column] = [
+                'name' => $column,
+                'type' => $this->getTypeFromColumnDescription($description),
+                'required' => $description['NULLABLE'] === false,
+                'arguments' => []
+            ];
         }
 
         return $config;
