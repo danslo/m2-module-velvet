94f89a19b44790a195c617c2fa814cf0d9f14fc2
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-07 21:27:16 +0100

    generate input types

--- Model/Config/TableReader.php
+++ Model/Config/TableReader.php
@@ -6,21 +6,29 @@ namespace Danslo\Velvet\Model\Config;
 
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\Config\ReaderInterface;
+use Magento\Framework\GraphQlSchemaStitching\GraphQlReader\Reader\InputObjectType;
+use Magento\Framework\GraphQlSchemaStitching\GraphQlReader\Reader\ObjectType;
 
 class TableReader implements ReaderInterface
 {
     private ResourceConnection $resourceConnection;
     private string $tableName;
     private string $schemaType;
+    private bool $generateInputType;
+    private array $nullableInputFields;
 
     public function __construct(
         ResourceConnection $resourceConnection,
         string $tableName,
-        string $schemaType
+        string $schemaType,
+        bool $generateInputType = true,
+        array $nullableInputFields = []
     ) {
         $this->resourceConnection = $resourceConnection;
         $this->tableName = $tableName;
         $this->schemaType = $schemaType;
+        $this->generateInputType = $generateInputType;
+        $this->nullableInputFields = $nullableInputFields;
     }
 
     private function getTypeFromColumnDescription(array $description): String
@@ -40,6 +48,38 @@ class TableReader implements ReaderInterface
         }
     }
 
+    private function getSchemaConfig(
+        string $schemaType,
+        string $graphqlType = ObjectType::GRAPHQL_TYPE,
+        bool $isInputType = false
+    ): array {
+        $config = [
+            'name' => $schemaType,
+            'type' => $graphqlType,
+            'fields' => []
+        ];
+
+        $tableDescription = $this->resourceConnection->getConnection()->describeTable($this->tableName);
+        foreach ($tableDescription as $column => $description) {
+            $columnIsRequired = $description['NULLABLE'] === false;
+
+            if ($isInputType) {
+                if (in_array($column, $this->nullableInputFields) || $description['PRIMARY'] === true) {
+                    $columnIsRequired = false;
+                }
+            }
+
+            $config['fields'][$column] = [
+                'name' => $column,
+                'type' => $this->getTypeFromColumnDescription($description),
+                'required' => $columnIsRequired,
+                'arguments' => []
+            ];
+        }
+
+        return $config;
+    }
+
     public function read($scope = null)
     {
         $connection = $this->resourceConnection->getConnection();
@@ -47,16 +87,13 @@ class TableReader implements ReaderInterface
             return [];
         }
 
-        $config = [];
-        foreach ($connection->describeTable($this->tableName) as $column => $description) {
-            $config[$this->schemaType]['fields'][$column] = [
-                'name' => $column,
-                'type' => $this->getTypeFromColumnDescription($description),
-                'required' => $description['NULLABLE'] === false,
-                'arguments' => []
-            ];
+        $types = [$this->schemaType => $this->getSchemaConfig($this->schemaType)];
+
+        if ($this->generateInputType) {
+            $inputType = $this->schemaType . 'Input';
+            $types[$inputType] = $this->getSchemaConfig($inputType, InputObjectType::GRAPHQL_INPUT, true);
         }
 
-        return $config;
+        return $types;
     }
 }
